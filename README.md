08ecfa0f32ab561a2a5b8f8bc22c195326c32cc9
YSD <18835948464@163.com>
2019-08-05 17:16:15 +0800

    模块的输出和获取

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # nodeJS
 nodeJS的基础学习
+# 模块
+要在模块中对外输出变量，用：
+
+module.exports = variable;
+输出的变量可以是任意对象、函数、数组等等。
+
+要引入其他模块输出的对象，用：
+
+var foo = require('other_module');
+引入的对象具体是什么，取决于引入模块输出的对象。
--- hello.js
+++ hello.js
@@ -1,4 +1,14 @@
-var name = 'duzekun';
+// var name = 'duzekun';
 
-var s = 'hello,'+name;
-console.log(s)
+// var s = 'hello,'+name;
+// console.log(s)
+
+
+var u = 'Hello';
+
+function greet(name) {
+    console.log(u + ', ' + name + '!');
+}
+
+module.exports = greet;
+
